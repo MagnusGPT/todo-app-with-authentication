891aea4a69e64192995de9df9e9b2fe3394a3805
Magnus The Developer <95050512+MagnusGPT@users.noreply.github.com>
2026-08-01 17:23:30 +0300

    Organized routing. Added USERS table and added relations between TODOS and USERS table. Requires auth addition. The program won't work right now.

--- db.js
+++ db.js
@@ -2,11 +2,26 @@ const Database = require('better-sqlite3');
 
 const db = new Database("todos.db");
 
+// Enable foreign key support
+db.pragma("foreign_keys = ON");
+
 db.exec(`
+    CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        username TEXT NOT NULL UNIQUE,
+        passwordHash TEXT NOT NULL
+    );
+
     CREATE TABLE IF NOT EXISTS todos (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         message TEXT NOT NULL,
-        isDone BOOLEAN NOT NULL
+        isDone INTEGER NOT NULL DEFAULT 0,
+
+        userId INTEGER NOT NULL,
+
+        FOREIGN KEY (userId)
+            REFERENCES users(id)
+            ON DELETE CASCADE
     );
 `);
 
---
+++ routes/todos.js
@@ -0,0 +1,186 @@
+const express = require("express");
+const db = require("../db");
+
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    const todos = db
+    .prepare('SELECT * FROM todos;')
+    .all();
+
+    const formattedTodos = todos.map(todo => ({
+        ...todo,
+        isDone: Boolean(todo.isDone)
+    }));
+
+    return res.status(201).json({
+        "data": formattedTodos
+    });
+});
+
+router.get("/:id", (req, res) => {
+    const id = Number(req.params.id);
+
+    if (isNaN(id)) {
+        return res.status(400).json({
+            message: "Invalid ID."
+        });
+    }
+    
+    const stmt = db
+    .prepare(`
+        SELECT * FROM todos
+        WHERE id = ?
+    `);
+
+    const todo = stmt.get(id);
+
+    if (!todo) {
+        return res.status(404).json({
+            "message": "Todo not found."
+        });
+    }
+    
+    todo.isDone = Boolean(todo.isDone);
+
+    res.json(todo);
+});
+
+router.post("/", (req, res) => {
+    const allowedFields = ["message"];
+
+    const keys = Object.keys(req.body);
+
+    const hasOnlyAllowedFields = keys.every(key => allowedFields.includes(key));
+
+    if (!hasOnlyAllowedFields) {
+        return res.status(400).json({
+            message: "Request contains invalid fields."
+        });
+    }
+
+    if (!("message" in req.body)) {
+        return res.status(400).json({
+            message: "message is required."
+        });
+    }
+
+    if (typeof req.body.message !== "string") {
+        return res.status(400).json({
+            message: "message must be a string."
+        });
+    }
+
+    const stmt = db.prepare(`
+        INSERT INTO todos
+        (message, isDone)
+        VALUES (?, ?)
+    `);
+
+    stmt.run(
+       req.body.message,
+       0
+    );
+
+    res.status(201).json({ 
+        message: "Todo created successfully!"
+    });
+});
+
+router.put("/:id", (req, res) => {
+
+    const id = Number(req.params.id);
+
+    if (isNaN(id)) {
+        return res.status(400).json({
+            message: "Invalid ID."
+        });
+    }
+
+    const allowedFields = ["message", "isDone"];
+
+    const keys = Object.keys(req.body);
+
+    const hasOnlyAllowedFields = keys.every(key =>
+        allowedFields.includes(key)
+    );
+
+    if (!hasOnlyAllowedFields) {
+        return res.status(400).json({
+            message: "Request contains invalid fields."
+        });
+    }
+
+    if (!("message" in req.body) || !("isDone" in req.body)) {
+        return res.status(400).json({
+            message: "message and isDone are required."
+        });
+    }
+
+    if (typeof req.body.message !== "string") {
+        return res.status(400).json({
+            message: "message must be a string."
+        });
+    }
+
+    if (typeof req.body.isDone !== "boolean") {
+        return res.status(400).json({
+            message: "isDone must be a boolean."
+        });
+    }
+
+    const stmt = db.prepare(`
+        UPDATE todos
+        SET
+            message = ?,
+            isDone = ?
+        WHERE id = ?
+    `);
+
+    const result = stmt.run(
+        req.body.message,
+        req.body.isDone ? 1 : 0,
+        id
+    );
+
+    if (result.changes === 0) {
+        return res.status(404).json({
+            message: "Todo not found."
+        });
+    }
+
+    res.json({
+        message: "Todo updated successfully."
+    });
+
+});
+
+router.delete("/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+    if (isNaN(id)) {
+        return res.status(400).json({
+            message: "Invalid ID."
+        });
+    }
+
+    const stmt = db.prepare(`
+        DELETE FROM todos 
+        WHERE id = ?
+    `);
+
+    const result = stmt.run(id);
+
+    //Did a row actually get deleted?
+    if (result.changes === 0) {
+        return res.status(404).json({
+            message: "Todo not found."
+        });
+    }
+
+    res.status(200).json({ 
+        message: "Todo deleted successfully!"
+    });
+});
+
+module.exports = router;
