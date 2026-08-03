cbcb51ccdc978395b0bbe7ebc11bb747e5d498a8
Magnus The Developer <95050512+MagnusGPT@users.noreply.github.com>
2026-08-03 18:30:31 +0300

    Added bcrypt to the stack. Completed /register endpoint with hashing.

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const db = require('../db');
+const bcrypt = require('bcrypt');
 const { validateCredentials } = require("../utils/validation");
 
 
@@ -9,7 +10,7 @@ router.post("/login", (req, res) => {
     
 });
 
-router.post("/register", (req, res) => {
+router.post("/register", async (req, res) => {
 
     if (!validateCredentials(req.body)) {
         return res.status(400).json({
@@ -17,9 +18,35 @@ router.post("/register", (req, res) => {
         });
     }
 
-    return res.status(200).json({
-        message: "Didn't do shit but congratz."
-    })
+    const { username, password } = req.body;
+
+    const passwordHash = await bcrypt.hash(password, 10);
+
+    try {
+        db.prepare(`
+            INSERT INTO users 
+            (username, password) 
+            VALUES 
+            (?, ?)`)
+        .run(
+            username, 
+            passwordHash
+        );
+        
+        return res.status(201).json({ 
+            message: "User created successfully." 
+        });
+    } 
+    catch (error) {
+        if (error.code === 'SQLITE_CONSTRAINT_UNIQUE') {
+            return res.status(409).json({ 
+                message: "This username already exists." 
+            });
+        }
+        return res.status(500).json({ 
+            message: "Internal server error." 
+        });
+    }
 });
 
 router.post("/logout", (req, res) => {
